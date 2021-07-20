2051f69f39106d512a30e8d02cdceb75084f4d5e
linamandla <linamabdlajojwana96@gmail.com>
2021-07-20 16:12:59 +0200

    changes

--- greet_xola.js
+++ greet_xola.js
@@ -3,11 +3,11 @@ const chalk = require('chalk');
 const greet = require('./greet');
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+
 
 var figlet = require('figlet');
  
-figlet('Hello Xola!!', function(err, data) {
+figlet('Hello Xola!!',function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
